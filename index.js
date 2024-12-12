b1734877f75088e2e0bbf3350903aa82aab31b73
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-12 09:07:32 +0600

    get all job data in database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { connection, client } = require("./DB/MongoDB");
+const { ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 connection();
@@ -14,6 +15,16 @@ const jobCollection = client.db("jobDB").collection("jobs");
 app.post("/jobs", async (req, res) => {
   const job = req.body;
   const result = await jobCollection.insertOne(job);
+  res.send(result);
+});
+app.get("/jobs", async (req, res) => {
+  const jobs = await jobCollection.find().toArray();
+  res.send(jobs);
+});
+app.get("/jobs/:id", async (req, res) => {
+  const id = req.params.id;
+  const params = { _id: new ObjectId(id) };
+  const result = await jobCollection.findOne(params);
   res.send(result);
 });
 app.get("/", (req, res) => {
