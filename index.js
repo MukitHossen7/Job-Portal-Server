6681a7d75df8f445de0c165261a01fb48cdc10a5
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-17 15:26:16 +0600

    vercel file add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { connection, client } = require("./DB/MongoDB");
 const { ObjectId } = require("mongodb");
 const app = express();
@@ -8,15 +10,57 @@ const port = process.env.PORT || 5000;
 connection();
 
 app.use(express.json());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-portal-bd647.web.app",
+      "https://job-portal-bd647.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 //custom middleware
-
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 //jobs related Api
 const jobCollection = client.db("jobDB").collection("jobs");
 const applyJobsCollection = client.db("jobDB").collection("applyJobs");
 
 //Auth related Api
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ tokenCreate: true });
+});
+
+app.post("/logout", (req, res) => {
+  res
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ tokenRemove: true });
+});
 
 //jobs related Api
 app.post("/jobs", async (req, res) => {
@@ -46,10 +90,12 @@ app.post("/apply_jobs", async (req, res) => {
   const result = await applyJobsCollection.insertOne(applyData);
   res.send(result);
 });
-app.get("/apply_jobs", async (req, res) => {
+app.get("/apply_jobs", verifyToken, async (req, res) => {
   const email = req.query.email;
   const query = { candidate_email: email };
-
+  if (req.user.email !== req.query.email) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
   const applyData = await applyJobsCollection.find(query).toArray();
   for (const application of applyData) {
     const params = { _id: new ObjectId(application.job_id) };
